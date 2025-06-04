a4f440b2bb82523537e6223579972a03154d787e
ArthurOliv2 <arthuroliveirap2014@gmail.com>
2025-06-04 18:36:43 -0300

    criação da pagina home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('title', 'Pagina Inicial')
+
+@section('content')
+    <div class="container text-center my-auto d-flex flex-column justify-content-center align-items-center flex-grow-1">
+        <h2 class="fw-bold">
+            Que tal simplificar a comunicação entre motoboy<br>
+             e empresa com o <span class="highlight">Motolink</span>?
+        </h2>
+        <p class="text-secondary fw-bold mt-3" style="max-width: 600px;">
+            Com o Motolink, sua empresa se comunica com motoboys de forma rapida e organizada, centralizando informações, reduzindo ruídos e ganhando tempo em cada entrega
+        </p>
+        <a href="#" class="btn btn-green mt-3 px-4 py-2">COMEÇAR HOJE MESMO!</a>
+    </div>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title', 'Motolink')</title>
+                
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+
+        <link href="{{ asset('css/home.css') }}" rel="stylesheet" >
+        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-dark px-3 d-flex justify-content-between align-items-center">
+        <span class="navbar-brand mb-0 h1">Motolink</span>
+        <div class="d-flex gap-2">
+            <a href="#" class="btn btn-outline-green me-2">CRIAR CONTA</a>
+            <a href="#" class="btn btn-green">LOGIN</a>
+        </div>
+    </nav>
+    
+    @yield('content')
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+
+    @stack('scripts')
+</body>
+</html>
